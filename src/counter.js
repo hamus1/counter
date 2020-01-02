b76d3775f0b9d36bb1176d41066c75b80eb9d3c7
hamus1 <mohamed.tounsi01@gmail.com>
2020-01-02 11:16:47 +0100

    debut avec erreur

---
+++ src/counter.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Counter extends React.Component {
+  render() {
+      constructor(props) 
+      super(props);
+      this.State = {
+          count:0
+      };
+    
+    return 
+    <div>
+        <button>plus</button>
+        <div></div>
+        <button>mines</button>
+    </div>;
+  }
+}
